70b901df8058c783f5c71cefd74a735f30978407
Julien Lerat <julien.lerat@csiro.au>
2023-11-29 16:30:09 +1100

    feat: added freq plots

---
+++ src/floodstan/freqplots.py
@@ -0,0 +1,122 @@
+import math, re
+from itertools import product as prod
+import numpy as np
+import pandas as pd
+
+from scipy.stats import norm
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import SymLogNorm
+
+from hydrodiy.stat import sutils
+
+PLOT_TYPES = ["gumbel", "normal"]
+
+def _check_plot_type(ptype):
+    txt = "/".join(PLOT_TYPES)
+    errmsg = f"Expected plot type in {txt}, got {ptype}."
+    assert ptype in PLOT_TYPES, errmsg
+
+
+def get_quantiles(prob, plot_type):
+    _check_plot_type(plot_type)
+    if plot_type == "gumbel":
+        return -np.log(-np.log(prob))
+    elif plot_type == "normal":
+        return norm.ppf(prob)
+
+
+def reduced_variate(nval, plot_type, cst=0.4):
+    """Generate reduced variates.
+
+    Parameters
+    ----------
+    nval : int
+        Number of points
+    plot_type : str
+        Plot type. See fplots.PLOT_TYPES.
+    cst : float
+        Parameter controlling the plotting position.
+        Set to 0.4 to use Cunnane's method (Cunnane, 1978).
+
+    Returns
+    -------
+    rvar : np.ndarray
+        1D array containing reduced variates.
+    """
+    ppos = sutils.ppos(nval, cst=0.4)
+    return get_quantiles(ppos, plot_type)
+
+
+def xaxis_label(ax, plot_type):
+    _check_plot_type(plot_type)
+    if plot_type == "gumbel":
+        ax.set_xlabel("Gumbel reduced variate [-]")
+    elif plot_type == "normal":
+        ax.set_xlabel("Standard normal deviate [-]")
+
+
+def add_aep_to_xaxis(ax, plot_type, full_line=True, return_periods=[5, 10, 50, 100, 200], \
+                        kwargs_plot={"color": "gray", "linewidth": 2}, \
+                        kwargs_text={"color": "gray", "va": "bottom", "ha": "center"}):
+    """ Add annual exceedance probabilities (AEP) to x axis.
+
+    Parameters
+    ----------
+    ax : matplotlib.Axes
+        Axe to draw on.
+    plot_type : str
+        Plot type. See fplots.PLOT_TYPES.
+    return_periods : list
+        List of reference return periods to plot.
+    """
+
+    aeps = 1/np.array(return_periods)*100
+    xpos = get_quantiles(1-aeps/100, plot_type)
+    y0, y1 = ax.get_ylim()
+    dy = (y1-y0)/50
+    nextline = "\n"
+    for retper, aep, x in zip(return_periods, aeps, xpos):
+        ax.plot([x, x], [y0, y0+dy], **kwargs_plot)
+        aep_txt = re.sub("\.0+$", "", f"{aep:0.2f}")
+        txt = f"{aep_txt}%{nextline}{retper:0.0f}Y"
+        ax.text(x, y0+2*dy, txt, **kwargs_text)
+
+        if full_line:
+            kwargs_plot_full = kwargs_plot.copy()
+            kwargs_plot_full["linewidth"] = kwargs_plot_full.get("linewidth", 2)/4
+            kwargs_plot_full["linestyle"] = kwargs_plot_full.get("linestyle", "--")
+            ax.plot([x, x], [y0, y1], **kwargs_plot_full)
+
+    ax.set_ylim((y0, y1))
+
+    return aeps, xpos
+
+
+def plot_data(ax, data, plot_type, **kwargs):
+    assert data.ndim == 1
+    data_sorted = np.sort(data)
+    nval = len(data)
+
+    rvar = reduced_variate(nval, plot_type)
+
+    kwargs["marker"] = kwargs.get("marker", "o")
+    kwargs["markerfacecolor"] = kwargs.get("markerfacecolor", "w")
+    kwargs["markeredgecolor"] = kwargs.get("markeredgecolor", "k")
+    kwargs["color"] = kwargs.get("color", "none")
+
+    ax.plot(rvar, data_sorted, **kwargs)
+
+    return rvar, data_sorted
+
+
+def plot_marginal(ax, marginal, plot_type, Tmin=1.1, Tmax=200, label="", **kwargs):
+    prob = np.linspace(1-1/Tmin, 1-1/Tmax, 500)
+    x = get_quantiles(prob, plot_type)
+    y = marginal.ppf(prob)
+
+    ax.plot(x, y, label=label, **kwargs)
+
+    return x, y
+
+
---
+++ tests/test_freqplots.py
@@ -0,0 +1,74 @@
+import sys, re, math
+import numpy as np
+import pandas as pd
+from itertools import product as prod
+from itertools import combinations
+from pathlib import Path
+
+import pytest
+import warnings
+
+import matplotlib.pyplot as plt
+
+from floodstan import marginals, freqplots
+
+from test_sample import get_stationids, get_ams
+
+FTESTS = Path(__file__).resolve().parent
+FIMG = FTESTS / "images"
+FIMG.mkdir(exist_ok=True)
+
+def test_variate(allclose):
+    nval = 4
+    data = np.linspace(0, 1, nval)
+    for plot_type in freqplots.PLOT_TYPES:
+        rv = freqplots.reduced_variate(nval, plot_type)
+        if plot_type == "gumbel":
+            expected = np.array([-0.66572981,  0.03554335,  0.73485899,  1.86982471])
+        elif plot_type == "normal":
+            expected = np.array([-1.06757052, -0.30298045,  0.30298045,  1.06757052])
+
+        assert allclose(rv, expected)
+
+
+def test_reduced_variate_gumbel(allclose):
+    nval = 20
+    rv = freqplots.reduced_variate(nval, "gumbel")
+    prob = (np.arange(1, nval+1)-0.4)/(nval+1-0.8)
+    expected = -np.log(-np.log(prob))
+    assert allclose(rv, expected, rtol=0, atol=1e-2)
+
+
+def test_plot_data():
+    streamflow = get_ams("203014")
+    for plot_type in freqplots.PLOT_TYPES:
+        plt.close("all")
+        fig, ax = plt.subplots()
+        freqplots.plot_data(ax, streamflow, plot_type)
+        freqplots.add_aep_to_xaxis(ax, plot_type)
+        fp = FIMG / f"freqlots_data_{plot_type}.png"
+        fig.savefig(fp)
+
+
+def test_plot_marginal():
+    streamflow = get_ams("203014")
+
+    plt.close("all")
+    fig, ax = plt.subplots()
+    ptype = "gumbel"
+    freqplots.plot_data(ax, streamflow, ptype)
+
+    gev = marginals.GEV()
+    for eta in range(4):
+        gev.fit_lh_moments(streamflow, eta)
+        lab = f"GEV $\eta$={eta}"
+        freqplots.plot_marginal(ax, gev, ptype, label=lab, Tmax=500)
+
+    retp = [5, 10, 100, 500]
+    aeps, xpos = freqplots.add_aep_to_xaxis(ax, ptype, retp)
+
+    ax.legend()
+    fp = FIMG/ "freqplots_marginal.png"
+    fig.savefig(fp)
+
+
