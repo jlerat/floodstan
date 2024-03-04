bc84a2d7e2394a9297f75a582419db944e2dc324
Julien Lerat <julien.lerat@csiro.au>
2024-03-04 22:07:49 +1100

    fix: improved freqplots aep marks

--- src/floodstan/freqplots.py
+++ src/floodstan/freqplots.py
@@ -74,14 +74,20 @@ def add_aep_to_xaxis(ax, plot_type, full_line=True, \
 
     aeps = 1/np.array(return_periods)*100
     xpos = get_quantiles(1-aeps/100, plot_type)
+
+    # Handle non-linear axis transforms
+    delta = 0.02
     y0, y1 = ax.get_ylim()
-    dy = (y1-y0)/50
+    fun = (ax.transAxes + ax.transData.inverted()).transform
+    _, y0d1 = fun((0, delta))
+    _, y0d2 = fun((0, 2*delta))
+
     nextline = "\n"
     for retper, aep, x in zip(return_periods, aeps, xpos):
-        ax.plot([x, x], [y0, y0+dy], **kwargs_plot)
+        ax.plot([x, x], [y0, y0d1], **kwargs_plot)
         aep_txt = re.sub("\.0+$", "", f"{aep:0.2f}")
         txt = f"{aep_txt}%{nextline}{retper:0.0f}Y"
-        ax.text(x, y0+2*dy, txt, **kwargs_text)
+        ax.text(x, y0d2, txt, **kwargs_text)
 
         if full_line:
             kwargs_plot_full = kwargs_plot.copy()
@@ -114,14 +120,20 @@ def plot_data(ax, data, plot_type, **kwargs):
 def plot_marginal(ax, marginal, plot_type, params=None, Tmin=1.1, Tmax=200, \
                         label="", coverage=0.9, truncated_probability=0., \
                         color="tab:blue", edgecolor="none", \
-                        facecolor="tab:blue", alpha=0.5, \
+                        facecolor="none", alpha=0.5, y_min_clip=0., \
                         **kwargs):
     # Compute probabilitie including censoring shift
     prob = np.linspace(1-1/Tmin, 1-1/Tmax, 500)
     probtrunc = (prob-truncated_probability)/(1-truncated_probability)
-
     x = get_quantiles(prob, plot_type)
+
+    if not params is None:
+        assert len(params)>0, "0 parameter sets provided"
+
+    # Colors
     kwargs["color"] = kwargs.get("color", color)
+    facecolor = color if facecolor=="none" else facecolor
+
     if params is None:
         ys = marginal.ppf(probtrunc)
         ax.plot(x, ys, label=label, **kwargs)
@@ -134,17 +146,18 @@ def plot_marginal(ax, marginal, plot_type, params=None, Tmin=1.1, Tmax=200, \
             ys.append(marginal.ppf(probtrunc))
 
         ys = pd.DataFrame(ys).T
-        ym = ys.mean(axis=1)
-
-        qq = (1-coverage)/2
-        yq1 = ys.quantile(qq, axis=1)
-        yq2 = ys.quantile(1-qq, axis=1)
-
+        ym = ys.mean(axis=1).clip(y_min_clip)
         ax.plot(x, ym, label=label, **kwargs)
-        ax.fill_between(x, yq1, yq2, \
-                            edgecolor=edgecolor, \
-                            facecolor=facecolor, \
-                            alpha=0.5)
+
+        if coverage>0:
+            qq = (1-coverage)/2
+            yq1 = ys.quantile(qq, axis=1).clip(y_min_clip)
+            yq2 = ys.quantile(1-qq, axis=1).clip(y_min_clip)
+
+            ax.fill_between(x, yq1, yq2, \
+                                edgecolor=edgecolor, \
+                                facecolor=facecolor, \
+                                alpha=alpha)
     return x, ys
 
 
